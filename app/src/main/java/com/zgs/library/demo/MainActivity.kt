95449f767855df6481da199d5ff15ddd8847a6a9
zgs <37368562@qq.com>
2021-01-26 16:40:45 +0800

    no message

--- app/src/main/java/com/zgs/library/demo/MainActivity.kt
+++ app/src/main/java/com/zgs/library/demo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.zgs.library.demo.base.activity.BaseActivity
 import com.zgs.library.demo.databinding.ActivityMainBinding
 
+
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun layoutId(): Int = R.layout.activity_main
 
--- app/src/main/java/com/zgs/library/demo/base/fragment/BaseFragment.kt
+++ app/src/main/java/com/zgs/library/demo/base/fragment/BaseFragment.kt
@@ -3,6 +3,7 @@ package com.zgs.library.demo.base.fragment
 import com.zgs.library.base.fragment.BaseDbLibFragment
 import com.zgs.library.demo.databinding.FragmentMainBinding
 
+
 /**
  * 作者　: zgs
  * 时间　: 2021/1/25
